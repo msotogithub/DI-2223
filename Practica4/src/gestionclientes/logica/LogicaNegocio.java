4a90f5d328bfe1da65cff3350c8eade520bf291f
Manuel Soto <manuel.soto.baquero@gmail.com>
2022-10-24 19:30:59 +0200

    Práctica 4 - añadir paquete con lógica

---
+++ Practica4/src/gestionclientes/logica/LogicaNegocio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionclientes.logica;
+import gestionclientes.dto.Cliente;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Manu
+ */
+public class LogicaNegocio {
+    private static List<Cliente> listaClientes = new ArrayList<>();
+    
+    public static void aniadirCliente(Cliente cliente)
+    {
+        listaClientes.add(cliente);
+    }
+    
+    public static List<Cliente> getListaClientes()
+    {
+        return listaClientes;
+    }
+}
