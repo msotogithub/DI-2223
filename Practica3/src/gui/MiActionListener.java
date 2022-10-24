d4591ce11ea9a40f83922a01a71cc08cb2c1052c
Manuel Soto <manuel.soto.baquero@gmail.com>
2022-10-24 18:56:23 +0200

    Práctica 3 - Lista

---
+++ Practica3/src/gui/MiActionListener.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Manu
+ */
+public class MiActionListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Se ha lanzado MiActionListener");
+    }
+    
+}
